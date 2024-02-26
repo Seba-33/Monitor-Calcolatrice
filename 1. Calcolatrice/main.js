bda43bc56dec900acf92bcb725bc14182b2f503b
net <net@INF-222.igalvani.local>
2024-02-26 13:33:15 +0100

    Initial Commit

--- 1. Calcolatrice/main.js
+++ 1. Calcolatrice/main.js
@@ -7,6 +7,28 @@ function sommaValore() {
     var primo = parseFloat(primoNumero.value);
     var secondo = parseFloat(secondoNumero.value);
     var somma = primo + secondo;
+    var sottrazione = primo - secondo;
+    var moltiplicazione = primo * secondo;
+    var divisione = primo / secondo;
   
     document.getElementById('somma').value = somma;
+    document.getElementById('sottrazione').value = sottrazione;
+    document.getElementById('moltiplicazione').value = moltiplicazione;
+    document.getElementById('divisione').value = divisione;
   }
+  let monitorAcceso = false;
+
+  function accendiMonitor() {
+      const monitor = document.getElementById('monitor');
+      const bottone = document.getElementById('bottone');
+      if (monitorAcceso){
+      monitor.src = "MonitorSpento.jpg";
+      bottone.textContent = 'Accendi monitor';
+      monitorAcceso = false
+      }
+      else{
+        monitor.src = "MonitorAcceso.jpg"
+        bottone.textContent = 'Spegni monitor';
+        monitorAcceso = true;
+      }
+    }
